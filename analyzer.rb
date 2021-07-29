403dad7a9d9bc655b9d9ec6edfdbb2abddc8578e
JudeuXD <joaogt13@gmail.com>
2021-07-29 12:30:15 -0300

    A text analyzer on Ruby

---
+++ analyzer.rb
@@ -0,0 +1,12 @@
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+
+puts "The text has #{line_count} line(s)"
+
+total_characters = text.length
+puts "The text has #{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "the text has #{total_characters_nospaces} characters excluding spaces"
+
