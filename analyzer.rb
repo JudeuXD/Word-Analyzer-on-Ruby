45722d2480a26d8c758a58b23547eed1d87a39b2
JudeuXD <joaogt13@gmail.com>
2021-07-29 12:47:07 -0300

    Add a word counter

--- analyzer.rb
+++ analyzer.rb
@@ -10,3 +10,5 @@ puts "The text has #{total_characters} characters"
 total_characters_nospaces = text.gsub(/\s+/, '').length
 puts "the text has #{total_characters_nospaces} characters excluding spaces"
 
+word_count = text.split.length
+puts "the text has #{word_count} words"
