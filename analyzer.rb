7ade1b4dd13937ad299b4bbe8186d19036bd5aa1
JudeuXD <joaogt13@gmail.com>
2021-08-05 11:41:05 -0300

    Final program with comments

--- analyzer.rb
+++ analyzer.rb
@@ -1,14 +1,27 @@
-lines = File.readlines("text.txt")
-line_count = lines.size
-text = lines.join
+#Author João Victor Santos Andrade AKA: "Judeu"
+#Date: August 5, 2021
+#Example from Beginning Ruby - From Novice to profssional by Peter Cooper
 
-puts "The text has #{line_count} line(s)"
+lines = File.readlines("text.txt") #receve a file .txt to be analyzed.
+line_count = lines.size # a kernel fuction to count the lines os the text
+text = lines.join #Put the text into array.
+
+puts "The text has #{line_count} line(s)" 
 
 total_characters = text.length
 puts "The text has #{total_characters} characters"
 
 total_characters_nospaces = text.gsub(/\s+/, '').length
-puts "the text has #{total_characters_nospaces} characters excluding spaces"
+puts "The text has #{total_characters_nospaces} characters excluding spaces"
 
 word_count = text.split.length
-puts "the text has #{word_count} words"
+puts "The text has #{word_count} words"
+
+paragraph_count = text.split(/\n\n/).length
+puts "The text has #{paragraph_count} paragraphs"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "The text has #{sentence_count} sentences"
+
+
+
